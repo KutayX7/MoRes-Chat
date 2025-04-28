080c0d08e21ae75457fe035bd69f3b26bde77f12
KutayX7 <103666634+KutayX7@users.noreply.github.com>
2025-04-29 01:18:56 +0300

    Add restorehistory command
    The new '/restorehistory' command restores the chat history when executed.

--- commands.py
+++ commands.py
@@ -1,7 +1,9 @@
-import utils
-import message_server
-from command import Command
 import config
+import message_server
+import utils
+from command import Command
+from message import Message
+from message_packet import MessagePacket
 
 commands: list[Command] = []
 
@@ -28,7 +30,7 @@ def command_help(*args: *tuple[str]) -> str:
             result.append('\n  ')
         return "".join(result)
 
-def encryption_command(*args: *tuple[str]):
+def encryption_command(*args: *tuple[str]) -> str:
     g_set = False
     p_set = False
     for i in range(len(args)):
@@ -65,11 +67,38 @@ def encryption_command(*args: *tuple[str]):
             config.PREFERRED_ENCRYPTION_LEVEL = 2
     return "Encryption paramters have been set."
 
+# TODO: Add option to restore most recent n messages
+# TODO: Add options to filter messages
+def restore_history(*args: *tuple[str]):
+    try:
+        with open('message_history.log', mode='r') as log:
+            while True:
+                line = log.readline()
+                if len(line) > 40:
+                    index_author = line.find('Author: ')
+                    index_message = line.find('Message: ')
+                    if index_message <= index_author:
+                        continue
+                    author = line[index_author+8:index_message-2]
+                    text = line[index_message+9:]
+                    if len(text) > 0 and author != '<system>':
+                        if text[-1] == '\n':
+                            text = text[:-1]
+                        if text[-1] == '\r':
+                            text = text[:-1]
+                        message_server.inbound_message_queue.put(MessagePacket(Message(author, text), ['<localhost>']))
+                else:
+                    break
+        return ""
+    except:
+        return "Failed to restore the message history."
+
 commands += [
     Command(command_help, ['/help', '/h'], "/help [command] : If provided, gives information about the specified command, otherwise returns information about all available commands. Aliases: /h"),
     Command(change_name, ['/username', '/u'], "/username <username> : Changes your username to <username>. Aliases: /u"),
     Command(exit, ['/exit', '/bye', '/goodbye'], "/exit : Closes the app. Aliases: /bye, /goodbye"),
     Command(encryption_command, ['/encryption', '/bye', '/goodbye'], "/encryption <parameter1> [parameter2]... : Sets encryption paramaters. These will not save between restarts.\n Parameters: \n\t -0 \t\t no encryption \n\t -1 \t\t default, diffie-hellman with preset g and p \n\t -2 \t\t diffie-hellman with custom g and p \n\t -g <N> \t\t sets DH_G to N \n\t -p <N> \t\t sets DH_P to N \n\t --fallback \t\t enabled by default, allows the connection to fallback to unencrypted plaintext \n\t --no-fallback \t\t opposite of --fallback"),
+    Command(restore_history, ['/restorehistory', '/rh'], "/restorehistory : Restores the entire message history from the 'message_history.log' file. Aliases: /rh")
 ]
 
 def run_command(text: str) -> bool:
@@ -81,8 +110,9 @@ def run_command(text: str) -> bool:
             result = command.execute(text)
         except:
             break
-        if result:
-            message_server.generate_system_message(result)
+        if (result != None) and (result != False):
+            if isinstance(result, str) and len(result) > 0:
+                message_server.generate_system_message(result)
             return True
     if text[0] == '/' and text[1] != ' ':
         message_server.generate_system_message("Unknown command. Please type '/help' for a list of available commands.")
